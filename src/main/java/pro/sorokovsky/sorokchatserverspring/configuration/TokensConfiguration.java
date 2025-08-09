585e070d9b3a470d94c9bcf55a423308ebb8c119
Sorokovsky <Sorokovskys@ukr.net>
2025-08-09 10:45:22 +0300

    tokens beans

--- src/main/java/pro/sorokovsky/sorokchatserverspring/configuration/TokensConfiguration.java
+++ src/main/java/pro/sorokovsky/sorokchatserverspring/configuration/TokensConfiguration.java
@@ -2,6 +2,7 @@ package pro.sorokovsky.sorokchatserverspring.configuration;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.KeyLengthException;
+import com.nimbusds.jose.crypto.DirectDecrypter;
 import com.nimbusds.jose.crypto.DirectEncrypter;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.MACVerifier;
@@ -10,11 +11,19 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pro.sorokovsky.sorokchatserverspring.deserializer.JweTokenDeserializer;
 import pro.sorokovsky.sorokchatserverspring.deserializer.JwsTokenDeserializer;
 import pro.sorokovsky.sorokchatserverspring.deserializer.TokenDeserializer;
+import pro.sorokovsky.sorokchatserverspring.factory.AccessTokenFactory;
+import pro.sorokovsky.sorokchatserverspring.factory.DefaultAccessTokenFactory;
+import pro.sorokovsky.sorokchatserverspring.factory.DefaultRefreshTokenFactory;
+import pro.sorokovsky.sorokchatserverspring.factory.RefreshTokenFactory;
 import pro.sorokovsky.sorokchatserverspring.serializer.JweTokenSerializer;
 import pro.sorokovsky.sorokchatserverspring.serializer.JwsTokenSerializer;
 import pro.sorokovsky.sorokchatserverspring.serializer.TokenSerializer;
+import pro.sorokovsky.sorokchatserverspring.storage.BearerTokenStorage;
+import pro.sorokovsky.sorokchatserverspring.storage.CookieTokenStorage;
+import pro.sorokovsky.sorokchatserverspring.storage.TokenStorage;
 
 import java.text.ParseException;
 
@@ -36,7 +45,37 @@ public class TokensConfiguration {
 
     @Bean
     @Qualifier("jwe-serializer")
-    public TokenSerializer jweTokenSerializer(@Value("${tokens.encryption-key}") String encryptionKey) throws ParseException, KeyLengthException {
+    public TokenSerializer jweTokenSerializer(@Value("${tokens.encryption-key}") String encryptionKey)
+            throws ParseException, KeyLengthException {
         return new JweTokenSerializer(new DirectEncrypter(OctetSequenceKey.parse(encryptionKey)));
+    }
+
+    @Bean
+    @Qualifier("jwe-deserializer")
+    public TokenDeserializer jweTokenDeserializer(@Value("${tokens.encryption-key}") String encryptionKey)
+            throws ParseException, KeyLengthException {
+        return new JweTokenDeserializer(new DirectDecrypter(OctetSequenceKey.parse(encryptionKey)));
+    }
+
+    @Bean
+    public AccessTokenFactory accessTokenFactory() {
+        return new DefaultAccessTokenFactory();
+    }
+
+    @Bean
+    public RefreshTokenFactory refreshTokenFactory() {
+        return new DefaultRefreshTokenFactory();
+    }
+
+    @Bean
+    @Qualifier("cookie-storage")
+    public TokenStorage cookieTokenStorage() {
+        return new CookieTokenStorage();
+    }
+
+    @Bean
+    @Qualifier("bearer-storage")
+    public TokenStorage bearerTokenStorage() {
+        return new BearerTokenStorage();
     }
 }
