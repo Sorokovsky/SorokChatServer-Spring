cb11fc613cccb1ce1afdbd4cc3e46e0844b31e55
Sorokovsky <Sorokovskys@ukr.net>
2025-08-14 16:37:29 +0300

    add swagger api info

--- src/main/java/pro/sorokovsky/sorokchatserverspring/pro/sorokovsky/sorokchatserverspring/configuration/SwaggerConfiguration.java
+++ src/main/java/pro/sorokovsky/sorokchatserverspring/pro/sorokovsky/sorokchatserverspring/configuration/SwaggerConfiguration.java
@@ -1,6 +1,7 @@
 package pro.sorokovsky.sorokchatserverspring.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,12 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfiguration {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI();
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .description("Бекенд для месенджера SorokChat")
+                                .title("Месенджер SorokChat")
+                                .version("1.0")
+                );
     }
 }
